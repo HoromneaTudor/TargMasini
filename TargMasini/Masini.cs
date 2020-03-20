36f3b53bf01aee6296ea4bdcc58ad0aad601fcce
Horomnea Tudor <tudor0123@gmail.com>
2020-03-20 18:48:26 +0200

    Add project files.

---
+++ TargMasini/Masini.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace targ_masini
+{
+    class masina
+    {
+        string marca;
+        int an;
+        int numar_pistoane;
+        int capacitate_cilindrica;
+        int nr_locuri;
+        string nume_vanzator;
+        string nume_cumparator;
+        string firma;
+        string model;
+
+        public masina()
+        {
+            nume_cumparator = string.Empty;
+            nume_vanzator = string.Empty;
+            model = string.Empty;
+            firma = string.Empty;
+        }
+        public masina(string numev, string numec, string firmaa, string modell)
+        {
+            nume_cumparator = numec;
+            nume_vanzator = numev;
+            firma = firmaa;
+            model = modell;
+
+        }
+
+        public masina(string nume1)
+        {
+            string[] buff = nume1.Split(",");
+            nume_cumparator = buff[0];
+            nume_vanzator = buff[1];
+            firma = buff[2];
+            model = buff[3];
+
+        }
+        public void afisare()
+        {
+            Console.WriteLine("Producator masina :{0}, model: {1}, nume cumparator: {2}, nume vanzator: {3}", firma, model, nume_cumparator, nume_vanzator);
+        }
+
+    }
+}
---
+++ TargMasini/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace targ_masini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new masina("alin", "petru", "opel", "astra");
+            f.afisare();
+            var f1 = new masina();
+            f1.afisare();
+            Console.ReadKey();
+            var f2 = new masina("tudor,narcis,citroen,c5");
+            f2.afisare();
+            Console.ReadKey();
+        }
+    }
+}
